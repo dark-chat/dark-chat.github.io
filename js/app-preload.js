b9558b884a26c29e69f087ca6c40e2b5f0bd5aa0
Tareq Ibrahim <idtareq@gmail.com>
2018-10-20 18:33:50 +0300

    update

--- js/app-preload.js
+++ js/app-preload.js
@@ -41,8 +41,23 @@ function setUpYoutubePlayer(initialVideoId) {
     }
 }
 
-function playVid(id) {
+function playVid(posted_msg) {
+    const rex_id = /(?:https?:\/\/)?(?:youtu\.be\/|(?:www\.|m\.)?youtube\.com\/(?:watch|v|embed)(?:\.php)?(?:\?.*v=|\/))([a-zA-Z0-9\-_]+)/;
+    const rex_playlist = /[&?]list=([^&]+)/i;
+    const res_id = posted_msg.match(rex_id);
+    const res_list = posted_msg.match(rex_playlist);
+    const yt_id = res_id[1];
+    const yt_list = res_list==null?null:res_list[1];
+
+
     player.setLoop(true);
-    player.loadPlaylist(id);
+    player.loadPlaylist(yt_id);
+
+    // if(yt_list){
+    //     player.loadPlaylist({list: yt_list, listType:'search'});
+    // }else{
+    //     player.loadPlaylist(yt_id);
+    // }
+
     player.setLoop(true);
 }
